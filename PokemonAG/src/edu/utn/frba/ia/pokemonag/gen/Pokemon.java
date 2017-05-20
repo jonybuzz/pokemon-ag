28906ba9319f4b6fde5b6940f8fc3120fbc55957
jonybuzz <buzzetti.jp@gmail.com>
2017-05-20 00:04:44 -0300

    Leer stats de un pokemon desde un archivo txt

--- PokemonAG/src/edu/utn/frba/ia/pokemonag/gen/Pokemon.java
+++ PokemonAG/src/edu/utn/frba/ia/pokemonag/gen/Pokemon.java
@@ -1,73 +1,79 @@
 package edu.utn.frba.ia.pokemonag.gen;
 
 import java.io.IOException;
-import java.util.Vector;
-import org.jgap.util.FileKit;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 public class Pokemon {
 
-    public byte tipo;
-    public byte ataque;
-    public byte ataqueEspecial;
-    public byte defensa;
-    public byte defensaEspecial;
+    public int tipo;
+    public int ataque;
+    public int ataqueEspecial;
+    public int defensa;
+    public int defensaEspecial;
 
-    public Pokemon(int idPokemon) throws IOException {
+    public Pokemon(int idPokemon) {
 
-        Vector pokemons = FileKit.readFile("pokemons.txt");
+        String stringStats;
 
-        String stringStats = (String) pokemons.get(idPokemon);
-
-        crearDesdeTexto(stringStats);
+        try (Stream<String> lines = Files.lines(Paths.get("pokemons.txt"))) {
+            stringStats = lines.skip(idPokemon).findFirst().get();
+            crearDesdeTexto(stringStats);
+        } catch (IOException ex) {
+            Logger.getLogger(Pokemon.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 
-    public byte getTipo() {
+    public int getTipo() {
         return tipo;
     }
 
-    public void setTipo(byte tipo) {
+    public void setTipo(int tipo) {
         this.tipo = tipo;
     }
 
-    public byte getAtaque() {
+    public int getAtaque() {
         return ataque;
     }
 
-    public void setAtaque(byte ataque) {
+    public void setAtaque(int ataque) {
         this.ataque = ataque;
     }
 
-    public byte getAtaqueEspecial() {
+    public int getAtaqueEspecial() {
         return ataqueEspecial;
     }
 
-    public void setAtaqueEspecial(byte ataqueEspecial) {
+    public void setAtaqueEspecial(int ataqueEspecial) {
         this.ataqueEspecial = ataqueEspecial;
     }
 
-    public byte getDefensa() {
+    public int getDefensa() {
         return defensa;
     }
 
-    public void setDefensa(byte defensa) {
+    public void setDefensa(int defensa) {
         this.defensa = defensa;
     }
 
-    public byte getDefensaEspecial() {
+    public int getDefensaEspecial() {
         return defensaEspecial;
     }
 
-    public void setDefensaEspecial(byte defensaEspecial) {
+    public void setDefensaEspecial(int defensaEspecial) {
         this.defensaEspecial = defensaEspecial;
     }
 
     private void crearDesdeTexto(String stringStats) {
-        
-        this.tipo = 50;
-        this.ataque = 50;
-        this.ataqueEspecial = 50;
-        this.defensa = 50;
-        this.defensaEspecial = 50;
+
+        this.tipo = Integer.parseInt(stringStats.substring(0, 3));
+        this.ataque = Integer.parseInt(stringStats.substring(3, 6));
+        this.ataqueEspecial = Integer.parseInt(stringStats.substring(6, 9));
+        this.defensa = Integer.parseInt(stringStats.substring(9, 12));
+        this.defensaEspecial = Integer.parseInt(stringStats.substring(12, 15));
     }
 }
