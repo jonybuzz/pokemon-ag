76ade8b02c782b96ffd247b5231e71e00653ca3d
Mauro Martinelli <maurolmartinelli@gmail.com>
2017-05-20 21:11:34 -0300

    Se lee el archivo una unica vez y se lo guarda en un array. Luego se accede a dicho array cuando se lo necesita.

--- src/main/java/edu/utn/frba/ia/pokemonag/poblacion/PoblacionEquipos.java
+++ src/main/java/edu/utn/frba/ia/pokemonag/poblacion/PoblacionEquipos.java
@@ -1,10 +1,15 @@
 package edu.utn.frba.ia.pokemonag.poblacion;
 
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Stream;
+import java.util.List;
+import java.util.ArrayList;
 import org.jgap.Chromosome;
 import org.jgap.Configuration;
 import org.jgap.IChromosome;
@@ -30,11 +35,34 @@ public class PoblacionEquipos {
      * @throws InvalidConfigurationException
      * @throws org.jgap.UnsupportedRepresentationException
      */
+
+
     public static Population crearRandom(Configuration config, IChromosome equipoDeMuestra)
             throws InvalidConfigurationException, UnsupportedRepresentationException {
 
         int tamanioPoblacion = config.getPopulationSize();
         Population poblacion = new Population(config, tamanioPoblacion);
+
+        BufferedReader reader = null;
+        List<String> linesArray = new ArrayList<String>();
+        try {
+            File file = new File("pokemons.txt");
+            reader = new BufferedReader(new FileReader(file));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                linesArray.add(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         //Crear poblacion leyendo lineas random del archivo txt
         for (int i = 0; i < tamanioPoblacion; i++) {
@@ -42,15 +70,10 @@ public class PoblacionEquipos {
             CompositeGene[] nuevosGenes = new CompositeGene[3];
 
             for (int j = 0; j < nuevosGenes.length; j++) {
-
                 nuevosGenes[j] = new CompositeGene(config, new IntegerGene(config));
                 int randomNum = ThreadLocalRandom.current().nextInt(0, 16 + 1);
-                
-                try (final Stream<String> lines = Files.lines(Paths.get("pokemons.txt"))) {
-                    String stringStats = lines.skip(randomNum).findFirst().get();
-                    nuevosGenes[j].setValueFromPersistentRepresentation(stringStats);
-                } catch (IOException ex) {
-                }
+                String randomNumberLine = linesArray.get(randomNum);
+                nuevosGenes[j].setValueFromPersistentRepresentation(randomNumberLine);
             }
 
             IChromosome cromosomaRandom = new Chromosome(config, nuevosGenes);
