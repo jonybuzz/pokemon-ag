261d87afd7ce1e3edeee9bceeedd39d8bf76630e
jonybuzz <buzzetti.jp@gmail.com>
2017-05-21 23:41:54 -0300

    Logueo de la ultima poblacion

--- src/main/java/PokeMain.java
+++ src/main/java/PokeMain.java
@@ -29,53 +29,51 @@ public class PokeMain {
             //cargar pokemones
             Pokemones pokemones = new Pokemones("pokemones.txt");
 
+            Configuration conf = new DefaultConfiguration();
+            conf.setPreservFittestIndividual(true);
+            conf.setKeepPopulationSizeConstant(false);
+            conf.setPopulationSize(TAMANO_POBLACION);
+
+            conf.getGeneticOperators().clear();
+            //cruza la mitad de los cromosomas
+            conf.addGeneticOperator(new CrossoverOperator(conf, TASA_DE_CRUZAMIENTO));
+            //muta 1 gen de cada 10
+            conf.addGeneticOperator(new MutationOperator(conf, TASA_DE_MUTACION));
+
+            conf.removeNaturalSelectors(true);
+            conf.addNaturalSelector(new WeightedRouletteSelector(conf), false);
+
+            //crear prototipo de chomosoma
+            Gene[] genes = new PokeGen[CANTIDAD_DE_POKEMONES_POR_EQUIPO];
+            for (int i = 0; i < CANTIDAD_DE_POKEMONES_POR_EQUIPO; i++) {
+                genes[i] = new PokeGen(conf, pokemones);
+            }
+            conf.setSampleChromosome(new Chromosome(conf, genes));
+
+            //equipo rival
+            Pokemon[] equipoRival = new Pokemon[CANTIDAD_DE_POKEMONES_POR_EQUIPO];
+            //120	Staryu	Agua	45	55	70	5
+            equipoRival[0] = pokemones.getPokemon(120 - 1);
+            //8	Wartortle	Agua	63	80	65	80
+            equipoRival[1] = pokemones.getPokemon(8 - 1);
+            //6	Charizard	Fuego	84	78	109	85
+            equipoRival[2] = pokemones.getPokemon(6 - 1);
+            //59	Arcanine	Fuego	110	80	100	80
+            equipoRival[3] = pokemones.getPokemon(59 - 1);
+            //133	Eevee	Normal	55	50	45	65
+            equipoRival[4] = pokemones.getPokemon(133 - 1);
+
+            System.out.println("Equipo a vencer:");
+            /* Mostrar equipo del gimnasio */
+            for (int i = 0; i < CANTIDAD_DE_POKEMONES_POR_EQUIPO; i++) {
+                System.out.println(equipoRival[i]);
+            }
+
+            //le paso el equipo a vencer a la funcion de aptitud
+            FitnessFunction fitness = new PokeFitnessFunction(equipoRival);
+            conf.setFitnessFunction(fitness);
+
             for (int c = 0; c < CANTIDAD_DE_CORRIDAS; c++) {
-                Configuration.reset();
-                Configuration conf = new DefaultConfiguration();
-                conf.setPreservFittestIndividual(true);
-                conf.setKeepPopulationSizeConstant(false);
-                conf.setPopulationSize(TAMANO_POBLACION);
-
-                conf.getGeneticOperators().clear();
-                //cruza la mitad de los cromosomas
-                conf.addGeneticOperator(new CrossoverOperator(conf, TASA_DE_CRUZAMIENTO));
-                //muta 1 gen de cada 10
-                conf.addGeneticOperator(new MutationOperator(conf, TASA_DE_MUTACION));
-
-                conf.removeNaturalSelectors(true);
-                conf.addNaturalSelector(new WeightedRouletteSelector(conf), false);
-
-                //crear prototipo de chomosoma
-                Gene[] genes = new PokeGen[CANTIDAD_DE_POKEMONES_POR_EQUIPO];
-                for (int i = 0; i < CANTIDAD_DE_POKEMONES_POR_EQUIPO; i++) {
-                    genes[i] = new PokeGen(conf, pokemones);
-                }
-                conf.setSampleChromosome(new Chromosome(conf, genes));
-
-                //equipo rival
-                Pokemon[] equipoRival = new Pokemon[CANTIDAD_DE_POKEMONES_POR_EQUIPO];
-                //120	Staryu	Agua	45	55	70	5
-                equipoRival[0] = pokemones.getPokemon(120 - 1);
-                //8	Wartortle	Agua	63	80	65	80
-                equipoRival[1] = pokemones.getPokemon(8 - 1);
-                //6	Charizard	Fuego	84	78	109	85
-                equipoRival[2] = pokemones.getPokemon(6 - 1);
-                //59	Arcanine	Fuego	110	80	100	80
-                equipoRival[3] = pokemones.getPokemon(59 - 1);
-                //133	Eevee	Normal	55	50	45	65
-                equipoRival[4] = pokemones.getPokemon(133 - 1);
-
-                if (c == 0) {
-                    System.out.println("Equipo a vencer:");
-                    /* Mostrar equipo del gimnasio */
-                    for (int i = 0; i < CANTIDAD_DE_POKEMONES_POR_EQUIPO; i++) {
-                        System.out.println(equipoRival[i]);
-                    }
-                }
-
-                //le paso el equipo a vencer a la funcion de aptitud
-                FitnessFunction fitness = new PokeFitnessFunction(equipoRival);
-                conf.setFitnessFunction(fitness);
 
                 Population pop = new Population(conf, conf.getPopulationSize());
                 RandomGenerator generator = conf.getRandomGenerator();
@@ -113,16 +111,26 @@ public class PokeMain {
                 double aptitudMejorEquipoIteracion = Double.MIN_VALUE;
                 Vector<Double> aptitudesIteracion = new Vector<Double>();
 
+                IChromosome equipoMasAptoCorrida = null;
+
                 for (int i = 0; i < CANTIDAD_DE_ITERACIONES; i++) {
                     population.evolve();
 
-                    double r = fitness.getFitnessValue(population.getFittestChromosome());
+                    equipoMasAptoCorrida = population.getFittestChromosome();
+                    double r = fitness.getFitnessValue(equipoMasAptoCorrida);
                     aptitudesIteracion.add(r);
                     if (r > aptitudMejorEquipoIteracion) {
                         aptitudMejorEquipoIteracion = r;
-                        mejorEquipoIteracion = population.getFittestChromosome();
+                        mejorEquipoIteracion = equipoMasAptoCorrida;
                     }
                 }
+
+                System.out.println("Mejor equipo de la ultima poblacion: ");
+                for (int i = 0; i < CANTIDAD_DE_POKEMONES_POR_EQUIPO; i++) {
+                    Pokemon pokemon = (Pokemon) ((PokeGen) equipoMasAptoCorrida.getGene(i)).getAllele();
+                    System.out.println(pokemon);
+                }
+                System.out.println("FA: " + fitness.getFitnessValue(equipoMasAptoCorrida));
 
                 if (aptitudMejorEquipoIteracion > aptitudMejorEquipoGlobal) {
                     aptitudMejorEquipoGlobal = aptitudMejorEquipoIteracion;
@@ -136,6 +144,8 @@ public class PokeMain {
                 Pokemon pokemon = (Pokemon) ((PokeGen) mejorEquipo.getGene(i)).getAllele();
                 System.out.println(pokemon);
             }
+            System.out.println("FA: " + fitness.getFitnessValue(mejorEquipo));
+
         } catch (Exception e) {
             System.out.println("JGAP -" + e.getMessage());
             throw new RuntimeException(e);
