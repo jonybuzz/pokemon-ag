3b7a16ce5452a01b7401aa846d4dc78673a62d72
jonybuzz <buzzetti.jp@gmail.com>
2017-05-21 21:21:10 -0300

    Logueo del tipo de pokemon en el resultado

--- src/main/java/Pokemon.java
+++ src/main/java/Pokemon.java
@@ -1,65 +1,75 @@
 
 public class Pokemon {
-		
-	private int id;
-	private String nombre;
-	private String tipo;
-	private int ataque;
-	private int ataqueEspecial;
-	private int defensa;
-	private int defensaEspecial;
-	
-	public Pokemon(int id, String nombre, String tipo, int ataque, int ataqueEspecial, int defensa, int defensaEspecial) {
-		this.id = id;
-		this.nombre = nombre;
-	 	this.tipo = tipo;
+
+    private int id;
+    private String nombre;
+    private String tipo;
+    private int ataque;
+    private int ataqueEspecial;
+    private int defensa;
+    private int defensaEspecial;
+
+    public Pokemon(int id, String nombre, String tipo, int ataque, int ataqueEspecial, int defensa, int defensaEspecial) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tipo = tipo;
         this.ataque = ataque;
         this.ataqueEspecial = ataqueEspecial;
         this.defensa = defensa;
         this.defensaEspecial = defensaEspecial;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	
-	public String getNombre() {
-		return nombre;
-	}
-		
-	public String getTipo() {
-		return tipo;
-	}
-		
-	public int getAtaque() {
-		return ataque;
-	}
-	
-	public void setAtaque(int ataque) {
-		this.ataque = ataque;
-	}
-		
-	public int getAtaqueEspecial() {
-		return ataqueEspecial;
-	}
-	
-	public void setAtaqueEspecial(int ataqueEspecial) {
-		this.ataqueEspecial = ataqueEspecial;
-	}
-		
-	public int getDefensa() {
-		return defensa;
-	}
-	
-	public void setDefensa(int defensa) {
-		this.defensa = defensa;
-	}
-		
-	public int getDefensaEspecial() {
-		return defensaEspecial;
-	}
-	
-	public void setDefensaEspecial(int defensaEspecial) {
-		this.defensaEspecial = defensaEspecial;
-	}
+    }
+
+    @Override
+    public String toString() {
+        return this.getNombre()
+                + " (" + this.getTipo() + ")"
+                + ", ataque: " + this.getAtaque()
+                + ", ataque especial: " + this.getAtaqueEspecial()
+                + ", defensa: " + this.getDefensa()
+                + ", defensa especial: " + this.getDefensaEspecial();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public int getAtaqueEspecial() {
+        return ataqueEspecial;
+    }
+
+    public void setAtaqueEspecial(int ataqueEspecial) {
+        this.ataqueEspecial = ataqueEspecial;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public int getDefensaEspecial() {
+        return defensaEspecial;
+    }
+
+    public void setDefensaEspecial(int defensaEspecial) {
+        this.defensaEspecial = defensaEspecial;
+    }
 }
